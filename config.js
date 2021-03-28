19ef44f239838ada6e4e85bf95436ad712a82efd
Glitch (hello-express) <none>
2021-03-28 18:46:12 +0000

    🛀📍 Checkpoint ./config.js:29848416/372

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ module.exports = {
   tokens: [
     "ODI1Nzc2MzQ2NDk1NzEzMzEz.YGDIsQ.vBij_8Tlf0--mHN922cKrKZ3TdQ",
     "ODI1NzgwNjM5OTUyNzk3NzE2.YGDJig.OdPdsebBKdZqRNL2Jyr7g2YR-tY",
-    "token",
-    "token",
-    "token",
-    "token",
+    "ODI1Nzg3Njg5ODgyMDkxNTkx.YGDAyQ.9OD9YyINrrFwC0NTHBgQmh4Ny6Y",
+    "ODI1Nzg4MjI4NjQ0NjM0NjM1.YGDBTw.iZhRCco1cBClGdT4VRJqqYv5EB8",
+    "ODI1Nzg4NjAzMjAzNDUyOTI5.YGDBzg.OU1qbDssOVjLYFN88Vlj3qBaboA",
+    "ODI1Nzg5NDcwNzQ1NjkwMTcy.YGDDEg.0yWXpMqIxHE1OQEoq-jqtE0Ptj0",
     "token",
     "token",
     "token",
@@ -18,7 +18,7 @@ module.exports = {
     "token",
     "token"
   ],
-  voice_channel: "797109650700238908",
-  text_channel: "750741284347445258",
-  your_server: "797109591207837706"
+  voice_channel: "825798986316251206",
+  text_channel: "824685477570674708",
+  your_server: "816964811451138098"
 };
